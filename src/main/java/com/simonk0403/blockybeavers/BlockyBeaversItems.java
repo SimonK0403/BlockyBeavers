1c7b7648cead78cb0e7391ad274f3f458eea620e
SimonK0403 <104595137+SimonK0403@users.noreply.github.com>
2024-09-07 17:33:45 +0200

    Added beaver drops - raw and cooked beaver

--- src/main/java/com/simonk0403/blockybeavers/BlockyBeaversItems.java
+++ src/main/java/com/simonk0403/blockybeavers/BlockyBeaversItems.java
@@ -1,6 +1,7 @@
 package com.simonk0403.blockybeavers;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.SpawnEggItem;
@@ -14,6 +15,16 @@ public class BlockyBeaversItems {
             "beaver_spawn_egg"
     );
 
+    public static final Item RAW_BEAVER = register(
+            new Item(new Item.Settings().food(new FoodComponent.Builder().nutrition(2).saturationModifier(0.3F).build())),
+            "raw_beaver"
+    );
+
+    public static final Item COOKED_BEAVER = register(
+            new Item(new Item.Settings().food(new FoodComponent.Builder().nutrition(6).saturationModifier(0.6F).build())),
+            "cooked_beaver"
+    );
+
     public static Item register(Item item, String id) {
         Identifier itemId = Identifier.of(BlockyBeavers.MOD_ID, id);
 
@@ -22,5 +33,7 @@ public class BlockyBeaversItems {
 
     public static void initialize() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register((itemGroup) -> itemGroup.add(BEAVER_SPAWN_EGG));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register((itemGroup) -> itemGroup.add(RAW_BEAVER));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register((itemGroup) -> itemGroup.add(COOKED_BEAVER));
     }
 }
